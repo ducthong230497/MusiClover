e13bfecd1b405f567e20e3601e6d35361dac6b25
ducthong230497 <15520856@gm.uit.edu.vn>
2018-09-23 20:51:51 +0700

    crawl data using javascript

--- app/scenes/testscene/Test.js
+++ app/scenes/testscene/Test.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react'
 import {View,Text ,StatusBar} from 'react-native'
 
-
 export default class Search extends Component{
 
     componentDidMount(){
         return fetch('https://www.nhaccuatui.com/flash/xml?html5=true&key1=1086ec88341c5050fc515be71ec1b844')
           .then((response) => {
-            console.log(response._bodyInit)
-            let re = new RegExp("<location>...</location>");
-            let a = re.test(response._bodyInit);
-            console.log(a);
-            re.test()
+            //console.log(response._bodyInit)
+            let re = /<location>\n\s\s\s\s\s\s\s\s(.*)\n\s\s\s\s<\/location>/ig;
+            
+            let result = response._bodyInit.match(re);
+            console.log(result[0])
+
+            let regexString = /(<([^>]+)>)/ig;
+
+            let cdataURL = result[0].match(regexString)
+            console.log(cdataURL[1])
+
+            let url = cdataURL[1].toString().replace("<![CDATA[", "").replace("]]>","")
+            console.log(url)
           }).catch((error) =>{
             console.error(error);
           });
