842de92f8877f77d8a9fa8873c58319f74d8e825
DucThong <thongnguyen230497@gmail.com>
2018-11-20 21:32:36 +0700

    nô thinh

--- app/scenes/testscene/Artist.js
+++ app/scenes/testscene/Artist.js
@@ -1,44 +1,40 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import SearchBar from 'react-native-searchbar'
 import { black } from 'ansi-colors';
+import {getTop100} from '../../connector/connector'
+import SongButton from '../_components/SongButton'
+import { Button } from 'react-native-elements';
 
 class ArtistScene extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {listArtist: []};
+        
+    }
+
     componentDidMount() {
-       
+        getTop100('https://www.nhaccuatui.com/playlist/top-100-pop-usuk-hay-nhat-va.zE23R7bc8e9X.html?st=1').then(result => {
+            this.setState({listArtist: result})
+            console.log(result)
+        })
+    }
+
+    renderArtist = ({item}) => {
+        <Text style = {styles.title}>{item.songName}</Text>
     }
 
     render() {
-        const data = [
-            { id: 1, label: 'Sơn Tùng M-TP' },
-            { id: 2, label: 'bảng xếp hạng' },
-            { id: 3, label: 'có ai thương em như anh' },
-            { id: 4, label: 'way back home' },
-            { id: 5, label: 'the show' },
-        ];
         return (
-            <View style={{backgroundColor:'white', flex: 1}}>
-                <View>
-                    <SearchBar
-                        // ref={(ref) => this.searchBar = ref}
-                        // data={items}
-                        // handleResults={this._handleResults}
-                        placeholder="Nhập từ khóa"
-                        placeholderTextColor='gray'
-                        fontSize={14}
-                        heightAdjust={-20}
-                        backCloseSize={20}
-                        onBack={() => this.props.navigation.goBack()}
-                        showOnLoad
-                    />
-                </View>
-                <View style={{paddingTop: 60, paddingLeft: 10}}>
-                    <Text style={styles.titleText}>TỪ KHÓA HOT</Text>
-                    
-                </View>
-            </View> 
-            
+            <View style = {styles.container}>
+                <FlatList
+                    data = {this.state.listArtist}
+                    renderItem = {this.renderArtist.bind(this)}
+                    keyExtractor = {item => item.songName}
+             
+                />
+            </View>
         )
     }
 }
@@ -50,5 +46,11 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingLeft: 16,
         paddingRight: 16,
-      }
+      },
+      title:{
+        fontSize: 12,
+        marginTop: 5,
+        marginBottom: 2,
+        color: 'white',
+    }
 })
